d6501eccf1c5e4e387179daf52ebd6d3508dff4f
Ajno <boyko.artiom@gmail.com>
2025-04-14 00:37:31 +0300

    Формы изменяемых жестов

--- dictionary/classes.py
+++ dictionary/classes.py
@@ -1,14 +1,52 @@
 import pandas as pd
+import numpy as np
+
+class UntreatableLexemeException(Exception): ...
 
 class Lexeme:
     def __init__(self, rows = pd.DataFrame):
         self.lemma, self.translations, self.sources = \
-            rows[rows["Lemma"].isna() | (rows["RSL"] == rows["Lemma"])].loc[0, ["RSL", "Russian", "Sources"]]
+            rows[rows["Lemma"].isna() | (rows["RSL"] == rows["Lemma"])].reset_index().loc[0, ["RSL", "Russian", "Sources"]]
         self.lemma: str
         self.translations: list[str] = self.translations.split(";")
-        self.sources: list[str] = self.sources.split(" ")
-        self.gram_forms = None
+        self.sources: list[str] = self.sources.split(" ") if self.sources is not np.nan else []
         if rows.shape[0] == 1:
             return
-        self.gram_forms = {}
-        self.gram_categories = {} # {S: [None я ты он], O: [мне тебе ему]}
+        self.inflection: list[dict[str, str | list[str]]] = [] # list of rows: [{"title", "value"}] or [{"title", "values"}]
+        self.inflection_columns: list[str] | None = None
+        categories: dict[str, list[str]] = {}
+        catval_to_word: dict[str, str] = {}
+        for word in rows.itertuples(index=False):
+            if word.Lemma is np.nan: continue
+            cat_val: list[str] = sorted(word.GrammarMeaning.split(";"))
+            catval_to_word[";".join(cat_val)] = word.RSL
+            cat_val: dict[str, str] = dict([tuple(cv.split(":")) for cv in cat_val])
+            if not categories:
+                categories = {cat: [val] for cat, val in cat_val.items()}
+            else:
+                for cat, val in cat_val.items():
+                    categories.setdefault(cat, [""])
+                    categories[cat].append(val)
+        self.inflection_dim: int = len(categories)           
+        if self.inflection_dim == 1:
+            cat, rows = categories.popitem()
+            for row in rows:
+                self.inflection.append({
+                    "title": row,
+                    "value": catval_to_word[f"{cat}:{row}"]
+                })
+        elif self.inflection_dim == 2:
+            cats = sorted(categories.items(), key = lambda i: len(i[1]))
+            colcat = cats[0][0]
+            rowcat = cats[1][0]
+            self.inflection_columns = categories[colcat]
+            for row in categories[rowcat]:
+                self.inflection.append({
+                    "title": row,
+                    "columns": [
+                        catval_to_word.get(";".join(sorted([f"{rowcat}:{row}", f"{colcat}:{col}"])))
+                        for col in categories[colcat]
+                    ]
+                })
+        else:
+            UntreatableLexemeException("Too many grammatical categories per lexeme")
--- dictionary/search.py
+++ dictionary/search.py
@@ -3,10 +3,12 @@ from .data import DICT
 from .classes import Lexeme
 
 def search_for_lexemes(query: str):
-    results = pd.concat([DICT[DICT["RSL"] == query], DICT[DICT["RSL"].str.contains(query)]], axis="index")
-    # results = DICT[DICT["RSL"] == query]
-    # results = DICT[DICT["RSL"].str.contains(query)]
-    results["Lemma"].fillna(results["RSL"], inplace=True)
+    results = pd.concat([
+        DICT[DICT["RSL"] == query], 
+        DICT[DICT["RSL"].str.contains(query, regex=False)]
+    ], axis="index")
+    results.drop_duplicates(inplace=True)
+    results.fillna({"Lemma": results["RSL"]}, inplace=True)
     lemmas = results["Lemma"].unique()
     search_results = [Lexeme(DICT[(DICT["Lemma"] == lemma) | (DICT["RSL"] == lemma)]) for lemma in lemmas]
     return search_results
