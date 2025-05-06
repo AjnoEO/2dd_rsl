1f138bb22ce90b79d14d977263dee66872091974
Ajno <boyko.artiom@gmail.com>
2025-05-06 23:41:50 +0300

    Исправил экранирование, из-за которого таблица считывалась неправильно

--- dictionary/data.py
+++ dictionary/data.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 DICT_PATH = os.path.join("dictionary", "dict.tsv")
-DICT = pd.read_csv(DICT_PATH, sep='\t')
+DICT = pd.read_csv(DICT_PATH, sep='\t', quotechar="'")
 
 def update_dict():
     DICT.to_csv(DICT_PATH, sep='\t')
