1bf95172f33cbf205e6a74294bb1bbc20aaacfee
Ajno <boyko.artiom@gmail.com>
2025-05-06 23:52:23 +0300

    Исправил упорядочивание результатов поиска

--- dictionary/search.py
+++ dictionary/search.py
@@ -6,15 +6,9 @@ from .classes import Lexeme
 def search_for_lexemes(query: str):
     similarity = DICT["RSL"].apply(lambda sign: sign_difference(sign, query)).rename("similarity")
     results = pd.concat([DICT, similarity], axis="columns")
-    results.sort_values(by=["similarity"])
-    # results = pd.concat([
-    #     DICT[DICT["RSL"] == query], 
-    #     DICT[DICT["RSL"].str.contains(query, regex=False)]
-    # ], axis="index")
-    # results.drop_duplicates(inplace=True)
+    results.sort_values(by=["similarity"], inplace=True)
     results = results[results["similarity"] < 3]
     results.fillna({"Lemma": results["RSL"]}, inplace=True)
     lemmas = results["Lemma"].drop_duplicates()
-    # lemmas = results["Lemma"].unique()
     search_results = [Lexeme(DICT[(DICT["Lemma"] == lemma) | (DICT["RSL"] == lemma)]) for lemma in lemmas]
     return search_results
